90717d4c0d8de0300cb7fca3aba39225321efdb5
Cody Bonsma <codybonsma@gmail.com>
2020-09-24 16:32:56 -0400

    downloaded inquirer package and created package-lock.json. linked fs and inquirer to the index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+// download packages (inquirer, fs)
+
+// capture user input using inquirer
+
+// create a readme file that populates the captured input 
+
+const fs = require("fs");
+const inquirer = require("requirer")
+
